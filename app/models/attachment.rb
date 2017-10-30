c4e98fa0a450a0a6400241605d449cb15ba0eb22
Pablo Barria Urenda <pablo.barria@post.harvard.edu>
2017-10-30 01:25:56 -0400

    removed (most likely redundant) paperclip configuration code from flood and attachment controller

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -13,15 +13,7 @@
 
 class Attachment < ActiveRecord::Base
   belongs_to :flood
-   has_attached_file :upload,
-  					:storage => :s3,
-  					:s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-  					:url => ":s3_domain_url",
-  					:path => ":attachment/:filename",
-  					:s3_host_name => "s3.amazonaws.com"
-  def s3_credentials
-    {:bucket => ENV["AWS_S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"], :s3_region => ENV["AWS_DEFAULT_REGION"]}
-  end
+  has_attached_file :upload
   validates_attachment_content_type :upload, :content_type =>['image/jpeg', 'image/png', 'image/gif', 'image/jpg', 'video/mp4', 'video/avi', 'video/mov']
 
 end
--- app/models/flood.rb
+++ app/models/flood.rb
@@ -2,17 +2,9 @@ class Flood < ActiveRecord::Base
   require 'csv'
   has_many :attachments
   accepts_nested_attributes_for :attachments
-  has_attached_file :attachment,
-  					:storage => :s3,
-  					:s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
-  validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
   validates_formatting_of :email, using: :email
   store :location, coder: JSON
-
-  def s3_credentials
-    {:bucket => ENV["AWS_S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"], :s3_region => ENV["AWS_DEFAULT_REGION"]}
-  end
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
