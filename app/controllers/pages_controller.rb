96ddbd3bfc5eb23127a53756d68e4f10a414773c
Chris Scott <cyberstrike@gmail.com>
2017-11-08 15:29:02 -0500

    fixed navbar, routes and pages controller, added bootstrap to willitflood page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,7 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-  layout 'applicationWillItFlood'
 
   def willitflood
-    
   end
 
-
-  private
-
-  def layout_for_page
-    id = params[:id]
-
-    return :home if id === 'home'
-    return false if id === 'willitflood'
-
-    :application
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'floods#index'
 
-
-
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
@@ -13,11 +11,9 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :pages
-  
   # Wild Card Catch-all for High Voltage
-  # get "/pages/*id" => 'pages#show', as: :page, format: false
+  get "/pages/*id" => 'pages#show', as: :page, format: false
 
-  # get 'willitflood' => 'pages#show', id: 'willitflood'
+  get 'willitflood' => 'pages#show', id: 'willitflood'
 
 end
